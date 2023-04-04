d0cb501c1c4dd1fd8a03e33cac3eaee9ef6de956
dunghenry <trandungksnb6930082001>
2023-04-04 16:54:38 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <Header></Header>
+    </header>
+    <main>
+        <slot></slot>
+    </main>
+    <footer>
+        <Footer></Footer>
+    </footer>
+</template>
+<script setup>
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+</script>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Post Item</h1>
+        <h2>{{ post.title }}</h2>
+    </div>
+</template>
+<script setup>
+let post = ref();
+const route = useRoute();
+//Block link
+await useAsyncData(() =>
+    $fetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`),
+).then((res) => {
+    // console.log(res.pending.value);
+    post = res.data.value;
+});
+//Not block link
+await useLazyAsyncData(() =>
+    $fetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`),
+).then((res) => {
+    // console.log(res.pending.value);
+});
+</script>
+<style></style>
